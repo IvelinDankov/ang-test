5312dd8f07d5258c222ee092532e7c009fbfcd58
Velin Dankov <velin80@web.de>
2024-11-06 16:04:44 +0200

    functionality add user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { SimpleUser } from './types';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'demo-app';
+  title = 'Demo-App';
 
-  handleClick() {
-    this.title = 'Здравей!';
+  changeTitle() {
+    this.title === 'Здравей! Демо Ап'
+      ? (this.title = 'Demo-App')
+      : (this.title = 'Здравей! Демо Ап');
+  }
+
+  userList: SimpleUser[] = [
+    { name: 'Ivan', age: 23 },
+    { name: 'Pesscho', age: 53 },
+    { name: 'Stilian', age: 43 },
+    { name: 'Bamba', age: 33 },
+    { name: 'Sam', age: 53 },
+  ];
+
+  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
+    this.userList.push({ name: inputName.value, age: Number(inputAge.value) });
+    inputName.value = '';
+    inputAge.value = '';
   }
 }
